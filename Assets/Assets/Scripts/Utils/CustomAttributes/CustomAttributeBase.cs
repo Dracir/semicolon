2331fc790be7cb0413e1e62a94a97d29af2ee6cc
straty91 <straty91@hotmail.com>
2014-08-14 00:11:10 -0400

    LE ménagee

--- Assets/Assets/Scripts/Utils/CustomAttributes/CustomAttributeBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class CustomAttributeBase : PropertyAttribute{
-	
-	public string PrefixLabel = "";
-	public bool NoPrefixLabel = false;
-	public bool NoFieldLabel = false;
-	public bool NoIndex = false;
-}
