548b3e709b3b5f7cdf06d7ed95b54638c9f649da
Richard <Rail.rich@gmail.com>
2014-09-25 15:36:42 -0400

    Full sexy refactor + New SpecialCharacterFactory qui fait des special factory de spikes!

---
+++ Assets/Editor/MapLoader/Factory/InstructionFactory.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class InstructionFactory  {
+
+	public static void createInstruction(string line, float x, float y, GameObject parent, Dictionary<string,string> parameters){
+		line = line.Replace('_',' ');
+
+		Instruction instruction = createInstructionObject(parent, line,x,y);
+
+		int indexOfArgument = line.IndexOf ('$');
+		int indexOfWhereImAt = 0;
+		int indexOfChild = 0;
+		while (indexOfArgument != -1) {
+			string argumentKey 	= line.Substring(indexOfArgument,3);
+			if(parameters.ContainsKey(argumentKey)){
+				string paramData 	= parameters[argumentKey];
+				setParameterData(instruction, indexOfChild++, paramData, parent);
+			}else{
+				Debug.LogError("Unknown parameter key\"" + argumentKey + "\"");
+			}
+			indexOfWhereImAt+= indexOfArgument + 1;
+			indexOfArgument = line.IndexOf ('$',indexOfArgument+1);
+		}
+		instruction.reset ();
+	}
+
+	private static Instruction createInstructionObject(GameObject parent, string line, float x, float y){
+		int indexOfArgument = line.IndexOf ('$');
+		while (indexOfArgument != -1) {
+			string argumentKey 	= line.Substring(indexOfArgument,3);
+			line = line.Replace(argumentKey,"$v");
+			indexOfArgument = line.IndexOf ('$',indexOfArgument+1);
+		}
+
+		GameObject go = GameObjectFactory.createGameObject (line,parent);
+		go.layer =  LayerMask.NameToLayer("Ignore Raycast");
+		go.transform.Translate (x, y, 0);
+
+		Instruction instruction = go.AddComponent<Instruction> ();
+		instruction.setText (line);
+
+		TextCollider2D tc = go.GetComponent<TextCollider2D> ();
+		tc.Font = GameConstantes.instance.statementFont;
+
+		return instruction;
+	}
+	
+	
+	private static void setParameterData(Instruction instruction, int childIndex, string paramData, GameObject parent){
+		string[] param = paramData.TrimEnd(new char[]{'\n','\r'}).Split(' ');
+		string type = param[0].ToLower();
+		string value = param[1].ToLower();
+		
+		if(type.Equals("boolean")){
+			instruction.setParameterTo(childIndex, DataType.BOOLEAN);
+			BooleanParameter boolean = instruction.GetChild(childIndex).GetComponent<BooleanParameter>();
+			if(value.StartsWith("true")){
+				boolean.Valeur = true;
+			}else if(value.StartsWith("false")){
+				boolean.Valeur = false;
+			}else{
+				Debug.LogError("MAPLOADER - ERROR : Unknown parameter value for " + paramData);
+			}
+		}else if(type.Equals("integer")){
+			instruction.setParameterTo(childIndex, DataType.INTEGER);
+			IntegerParameter integer = instruction.GetChild(childIndex).GetComponent<IntegerParameter>();
+			integer.Valeur = int.Parse(value);
+			if(param.Length >= 3){
+				if(param[2].StartsWith("setPlayerJumpHeight")){
+					GravityChanger gc = parent.AddComponent<GravityChanger>();
+					gc.gravityValue = integer;
+					integer.observers.Add(gc);
+				}
+			}
+		}else{
+			Debug.LogError("MAPLOADER - ERROR : Unknown parameter type for " + paramData);
+		}
+	}
+}
--- Assets/Editor/MapLoader/FileToLevelLoader.cs
+++ Assets/Editor/MapLoader/FileToLevelLoader.cs
@@ -8,7 +8,7 @@ public class FileToLevelLoader {
 
 	private ReadingMod readingMod;
 	private GameObject world;
-	private GameObject statements;
+	private GameObject instructions;
 
 	private float levelY;
 
@@ -19,7 +19,7 @@ public class FileToLevelLoader {
 
 	public void load(string mapText, GameObject world){	
 		this.parameters 				= new Dictionary<string, string>();
-		this.statements 				= GameObjectFactory.createGameObject ("Statements", world);
+		this.instructions 				= GameObjectFactory.createGameObject ("Instruction", world);
 		this.specialCharacter 			= new Dictionary<string, string>();
 		this.specialCharacterGameObject = new Dictionary<string, GameObject>();
 		this.world 						= world;
@@ -78,69 +78,29 @@ public class FileToLevelLoader {
 			if(lenght > 0){
 				string nextStatement = line.Substring(x,lenght);
 				if(nextStatement.Length != 0 && !nextStatement.Equals(" ")){
-					createInstruction(nextStatement,x,levelY);
+					create(nextStatement, x,levelY);
 					x += lenght;
 				}
-
 			}else{
 				x += 1;
 			}
-			
 		}
 		levelY-=1.66f;
 	}
 
-
-	private void createInstruction(string line, float x, float y){
-		line = line.Replace('_',' ');
-
-		Instruction instruction = createInstructionObject(line,x,y);
-
-		int indexOfArgument = line.IndexOf ('$');
-		int indexOfWhereImAt = 0;
-		int indexOfChild = 0;
-		while (indexOfArgument != -1) {
-			string argumentKey 	= line.Substring(indexOfArgument,3);
-			if(parameters.ContainsKey(argumentKey)){
-				string paramData 	= parameters[argumentKey];
-				setParameterData(instruction, indexOfChild++, paramData);
-			}else{
-				Debug.LogError("Unknown parameter key\"" + argumentKey + "\"");
-			}
-			indexOfWhereImAt+= indexOfArgument + 1;
-			indexOfArgument = line.IndexOf ('$',indexOfArgument+1);
-		}
-		instruction.reset ();
-	}
-
-	private Instruction createInstructionObject(string line, float x, float y){
-		int indexOfArgument = line.IndexOf ('$');
-		while (indexOfArgument != -1) {
-			string argumentKey 	= line.Substring(indexOfArgument,3);
-			line = line.Replace(argumentKey,"$v");
-			indexOfArgument = line.IndexOf ('$',indexOfArgument+1);
-		}
-		GameObject parent = this.statements;
+	void create(string line, int x, float y){
+		GameObject parent = this.instructions;
 		if(this.specialCharacter.ContainsKey(line)){
 			string[] sp = this.specialCharacter[line].TrimEnd(new char[]{'\n','\r'}).Split(' ');
 			parent = addOrGetParent(sp[0]);
-			
+			SpecialCharacterFactory.createSpecialCharacter(parent, line, this.specialCharacter[line], x, y);
+		}else{
+			InstructionFactory.createInstruction(line, x, y, this.instructions, this.parameters);
 		}
-
-		GameObject go = GameObjectFactory.createGameObject (line,parent);
-		go.layer =  LayerMask.NameToLayer("Ignore Raycast");
-		go.transform.Translate (x, y, 0);
-
-		Instruction instruction = go.AddComponent<Instruction> ();
-		instruction.setText (line);
-
-		TextCollider2D tc = go.GetComponent<TextCollider2D> ();
-		tc.Font = GameConstantes.instance.statementFont;
-
-		return instruction;
 	}
+	
 
-	GameObject addOrGetParent(string parentName){
+	private GameObject addOrGetParent(string parentName){
 		if(this.specialCharacterGameObject.ContainsKey(parentName)){
 			return this.specialCharacterGameObject[parentName];
 		}else{
@@ -149,35 +109,4 @@ public class FileToLevelLoader {
 			return parent;
 		}
 	}
-	private void setParameterData(Instruction instruction, int childIndex, string paramData){
-		string[] param = paramData.TrimEnd(new char[]{'\n','\r'}).Split(' ');
-		string type = param[0].ToLower();
-		string value = param[1].ToLower();
-		
-		if(type.Equals("boolean")){
-			instruction.setParameterTo(childIndex, DataType.BOOLEAN);
-			BooleanParameter boolean = instruction.GetChild(childIndex).GetComponent<BooleanParameter>();
-			if(value.StartsWith("true")){
-				boolean.Valeur = true;
-			}else if(value.StartsWith("false")){
-				boolean.Valeur = false;
-			}else{
-				Debug.LogError("MAPLOADER - ERROR : Unknown parameter value for " + paramData);
-			}
-		}else if(type.Equals("integer")){
-			instruction.setParameterTo(childIndex, DataType.INTEGER);
-			IntegerParameter integer = instruction.GetChild(childIndex).GetComponent<IntegerParameter>();
-			integer.Valeur = int.Parse(value);
-			if(param.Length >= 3){
-				if(param[2].StartsWith("setPlayerJumpHeight")){
-					GravityChanger gc = world.AddComponent<GravityChanger>();
-					gc.gravityValue = integer;
-					integer.observers.Add(gc);
-				}
-			}
-		}else{
-			Debug.LogError("MAPLOADER - ERROR : Unknown parameter type for " + paramData);
-		}
-	}
-
 }
