14eb6e0d795d840f99f50f8d9577fe28c867b872
Travis Henry Martin <travis.h.martin@gmail.com>
2014-10-21 11:15:04 -0400

    Adding basic UI stuff
    Signed-off-by: Travis Henry Martin <travis.h.martin@gmail.com>

---
+++ Assets/Scripts/TheGame/Arcade/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartGame () {
+		Application.LoadLevel("TravGym");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
